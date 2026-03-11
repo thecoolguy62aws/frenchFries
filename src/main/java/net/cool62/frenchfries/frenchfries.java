32a6ea704d581d1af08d043e85fa4a654bcb2eab
thecoolguy62aws <150736455+thecoolguy62aws@users.noreply.github.com>
2026-03-11 15:33:03 -0700

    chore: add minimal mod content

---
+++ src/main/java/net/cool62/frenchfries/frenchfries.java
@@ -0,0 +1,10 @@
+package net.cool62.frenchfries;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("frenchfries")
+public class frenchfries {
+    public frenchfries() {
+        // No code here yet!
+    }
+}
